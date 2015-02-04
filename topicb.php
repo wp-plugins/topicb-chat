073eab6cc8e3cab4b4ac0bc0f35a914344c0919b
bofrank <bofrank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-04 20:23:31 +0000

    html into description
    git-svn-id: https://plugins.svn.wordpress.org/topicb-chat/trunk@1082549 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- topicb.php
+++ topicb.php
@@ -6,13 +6,7 @@
 /*
 Plugin Name: TopicB
 Plugin URI: http://wordpress.org/plugins/topicb/
-Description: TopicB provides live conversations around your posts. TopicB is simple to install:\n1. Upload 'topicb.php' to the '/wp-content/plugins/' directory
-2. Activate the plugin through the 'Plugins' menu in WordPress
-
-TopicB Chat provides live conversations by automatically creating chat rooms for each post in your WordPress site.
-The chat is not recorded anywhere.
-The chat does not require a profile, login, handle and is anonymous although the option for a handle is provided.
-The chat room is only defined by the title of a post.
+Description: TopicB provides live conversations around your posts. TopicB is simple to install:<br/>1. Upload 'topicb.php' to the '/wp-content/plugins/' directory<br/>2. Activate the plugin through the 'Plugins' menu in WordPress<br/><br/>TopicB Chat provides live conversations by automatically creating chat rooms for each post in your WordPress site.<br/>The chat is not recorded anywhere.<br/>The chat does not require a profile, login, handle and is anonymous although the option for a handle is provided.<br/>The chat room is only defined by the title of a post.
 Author: Bolinas Frank
 Version: 0.4.7
 Author URI: http://topicb.com/
